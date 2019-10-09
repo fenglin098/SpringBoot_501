e58d1a6b4afa6350776ced04fb4184fe2fa94cc2
feng.lin098 <feng.lin098@gmail.com>
2019-10-09 14:23:18 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot501ExeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot501ExeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot501ExeApplication.class, args);
+    }
+
+}
